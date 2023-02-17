847a74514bff277bdb3989ad53f9bdbf55a91f12
Elliot Morrison <elliotmorrison58@gmail.com>
2023-02-16 20:09:25 -0500

    Installed ts log and updated the appbar

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -7,6 +7,7 @@ import { reactive } from 'vue';
 const form = reactive({
     email: '',
     password: '',
+    remember: false,
 });
 
 function login() {
@@ -19,18 +20,53 @@ function login() {
 
 <template>
     <HomeLayout>
-        <section class="grid md:grid-cols-2 mt-10">
-            <section>
-            </section>
-            <q-form @submit.prevent="login" class="mt-40">
+        <!-- <section class="flex justify-center items-center pt-40">
+             <section>
+            </section> -->
+            <!-- <section class="flex flex-col justify-center items-center w-full max-w-6xl bg-gray-800 h-80"> -->
+                
+                
+            <!-- </section> -->
+            <!-- <h1 class="text-4xl font-bold">Login</h1>
+                <p class="text-gray-200 font-bold">Login to your account</p>
+                <div class="bg-gray-800 p-5">
+                    <q-form @submit.prevent="login">
                 <div class="flex flex-col space-y-2">
                     <q-input v-model="form.email" label="Email"></q-input>
                     <q-input type="password" v-model="form.password" label="Password"></q-input>
                 </div>
                 <q-btn type="submit" class="full-width mt-3">Login</q-btn>
             </q-form>
+                </div> -->
 
-        </section>
+        <!-- </section> --> -->
+        <div class="h-full flex items-center justify-center">
+            <!-- <section class="flex flex-col justify-center items-center w-full max-w-6xl h-96"></section> -->
+		<div class="max-w-md w-full bg-gray-800 p-6 rounded-md shadow-lg">
+            
+			<h1 class="text-3xl font-bold text-center mb-6">Log In</h1>
+            <p class="text-gray-200 font-bold text-center">Login to your account</p>
+			<q-form class="space-y-6" @submit.prevent="login">
+				<div>
+					<q-input v-model="form.email" label="Email"></q-input>
+				</div>
+				<div>
+                    <q-input type="password" v-model="form.password" label="Password"></q-input>
+				</div>
+                <div class="flex items-center justify-between">
+                    <q-checkbox v-model="form.remember" label="Remember me" />
+                    <q-btn flat label="Forgot password?" />
+                </div>
+                <div class="flex items-center justify-between">
+                    <q-btn flat label="Don't have an account?" />
+                    <q-btn flat label="Register" />
+                </div>
+				<div>
+					<button class="w-full bg-indigo-500 text-white p-2 rounded-md hover:bg-indigo-600 transition-colors duration-300" type="submit">Log In</button>
+				</div>
+			</q-form>
+		</div>
+	</div>
 
     </HomeLayout>
 
