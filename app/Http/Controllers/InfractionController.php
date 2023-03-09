33767dedb57d0060acf88a3d42c15066c285299b
khalid sheriff <khalalimsheriff@gmail.com>
2023-03-08 23:46:46 -0500

    created InfractionController, then forgot i had to pack

---
+++ app/Http/Controllers/InfractionController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InfractionController extends Controller
+{
+    public function index(){
+
+    }
+
+    /* To do:
+    * - Everything: Authenticate for Admin
+    * - Create Infractions Page (Authenticate for Admin)
+    * - Create function(s) for infraction CRUD (See: Resource Controllers)
+    * - Add foreign key relationships with other entities (also, make *some* of the other necessary entities)
+    * - integrate model with activity logger
+    * - uhhh...
+    */
+}
