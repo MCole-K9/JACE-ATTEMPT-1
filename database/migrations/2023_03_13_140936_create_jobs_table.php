5a8615695386749e27f823bc2c83db34aae63ebb
MCole-K9 <m.b.cole300@gmail.com>
2023-03-13 10:34:04 -0500

    Created Job table and relevant changes to facilitate

---
+++ database/migrations/2023_03_13_140936_create_jobs_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('requirements');
+            $table->float('salary');
+            $table->boolean('is_visible');
+            $table->date('open_date');
+            $table->date('close_date');
+            $table->string('type');
+            $table->foreignId('org_rep_id')->constrained('org_reps');
+            $table->foreignId('organization_id')->constrained('organizations');
+            //$table->foreignId('job_category_id')->constrained('job_categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('jobs');
+    }
+};
