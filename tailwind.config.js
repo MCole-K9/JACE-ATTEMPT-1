34df2fea6265415a02d206f4ee02df39bd104095
MCole-K9 <m.b.cole300@gmail.com>
2023-03-14 16:46:15 -0500

    Changed Background Color to white

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,62 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('rippleui').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-      './resources/**/*.blade.php',
-      './resources/**/*.vue',
-  ],
-  content: [
-      './resources/**/*.blade.php',
-      './resources/**/*.vue',
-  ],
-  darkMode: false,
-  theme: {
-    extend: {},
-    screens: {
-      'xs': '320px',
-      // => @media (min-width: 320px) { ... }
-      
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+    mode: "jit",
+    purge: ["./resources/**/*.blade.php", "./resources/**/*.vue"],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.vue"],
+    darkMode: false,
+    theme: {
+        extend: {},
+        screens: {
+            xs: "320px",
+            // => @media (min-width: 320px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+            sm: "640px",
+            // => @media (min-width: 640px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+            md: "768px",
+            // => @media (min-width: 768px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    }
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require("rippleui")],
-  rippleui: {
-    themes: [
-			{
-				themeName: "light",
-				colorScheme: "light",
-        prefersColorScheme: true,
-				colors: {
-					primary: "#235264",
-					backgroundPrimary: "#964643",
-				},
-			},
-			{
-				themeName: "dark",
-				colorScheme: "dark",
-        prefersColorScheme: false,
-				colors: {
-					primary: "#573242",
-					backgroundPrimary: "#1a1a1a",
-				},
-			},
-		],
-  },
-}
+            xl: "1280px",
+            // => @media (min-width: 1280px) { ... }
+
+            "2xl": "1536px",
+            // => @media (min-width: 1536px) { ... }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("rippleui")],
+    rippleui: {
+        themes: [
+            {
+                themeName: "light",
+                colorScheme: "light",
+                prefersColorScheme: true,
+                colors: {
+                    primary: "#235264",
+                    backgroundPrimary: "#FFFFFF",
+                },
+            },
+            {
+                themeName: "dark",
+                colorScheme: "dark",
+                prefersColorScheme: false,
+                colors: {
+                    primary: "#573242",
+                    backgroundPrimary: "#1a1a1a",
+                },
+            },
+        ],
+    },
+};
