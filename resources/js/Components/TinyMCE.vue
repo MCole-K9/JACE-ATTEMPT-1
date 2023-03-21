69990d7bc1c7f4dfb1ec230955f7b77435fa8346
MCole-K9 <m.b.cole300@gmail.com>
2023-03-21 16:18:51 -0500

    Create Job

---
+++ resources/js/Components/TinyMCE.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import Editor from "@tinymce/tinymce-vue";
+import { tinyMCEConfig } from '../Lib/config';
+
+const props = defineProps<{
+    value: string;
+}>();
+
+const { value } = toRefs(props);
+
+</script>
+
+<template>
+    <Editor api-key="no-api-key" :init="tinyMCEConfig" v-model="value" />
+</template>
+
+
+
+<style scoped>
+
+</style>
