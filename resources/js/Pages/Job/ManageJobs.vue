c811b2f116b8619e00641d5e6b71b243c0e917f3
MCole-K9 <m.b.cole300@gmail.com>
2023-03-27 09:34:39 -0500

    Org Job Listing

--- resources/js/Pages/Job/ManageJobs.vue
+++ resources/js/Pages/Job/ManageJobs.vue
@@ -2,6 +2,11 @@
 import DashboardLayout from '../../Layout/DashboardLayout.vue';
 import { ref, defineProps, onMounted } from 'vue'
 import { Link, usePage } from '@inertiajs/vue3';
+import { Job } from '../../Lib/types';
+
+const props = defineProps<{
+    jobs: Job[]
+}>()
 
 </script>
 
@@ -10,13 +15,47 @@ import { Link, usePage } from '@inertiajs/vue3';
         <div class="flex justify-between items-center">
             <h1>Manage Jobs</h1>
             <Link href="/jobs/create">
-                <button class="btn btn-primary">Create Job</button>
+            <button class="btn btn-primary">Create Job</button>
             </Link>
+        </div>
+        <div class="flex w-full overflow-x-auto my-5">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Open Date</th>
+                        <th>Deadline</th>
+                        <th>Visibility</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="job in jobs" :key="job.id">
+                        <td>
+                            <Link :href="'/jobs/' + job.id">
+                                {{job.title}}
+                            </Link>
+                        </td>
+                        <td>
+                            {{job.open_date}}
+                        </td>
+                        <td>
+                            {{job.close_date}}
+                        </td>
+                        <td>
+                            {{job.is_visible ? 'Public' : 'Private'}}
+                        </td>
+                        <td>
+                            <Link :href="'/jobs/' + job.id + '/edit'">
+                                <button class="btn btn-sm btn-primary">Edit</button>
+                            </Link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </DashboardLayout>
 </template>
 
 
-<style scoped>
-
-</style>
+<style scoped></style>
