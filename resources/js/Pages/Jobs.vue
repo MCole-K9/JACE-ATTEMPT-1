ef6e1e20ed17fef7bdf04e4e4143b915a0ca5931
Elliot Morrison <elliotmorrison58@gmail.com>
2023-04-04 16:23:28 -0500

    updated jobs page

--- resources/js/Pages/Jobs.vue
+++ resources/js/Pages/Jobs.vue
@@ -1,8 +1,8 @@
 <template>
-    <HomeLayout background="bg-white">
-         <section class=" flex justify-center bgGold h-56 w-full items-center px-10">
-            <article class="prose text-center w-full">
-               <h1>Find the right job for you</h1>
+    <HomeLayout background="bgGold">
+         <section class=" flex justify-center bgBlue h-56 w-full items-center px-10">
+            <article class="prose text-center text-white w-full">
+               <h1 class="text-white">Find the right job for you</h1>
                <p>Search for jobs in your area and apply for the ones you like.</p>
             </article>
          </section>
@@ -67,6 +67,7 @@
                               <span>$100,000+</span>
                            </label>
                         </div>
+                        <button class="btn btn-block mt-5">Apply Filter</button>
                   </div>
                </div>
                <div class="h-1/1 p-5 bg-gray-200 xl:col-span-3 col-span-4 rounded-xl">
@@ -80,7 +81,7 @@
                   </section>
                   <section class="flex justify-center pt-5">
                      <div class="pagination ">
-                     <button class="btn" @click="pagination.previousPage" v-if="pagination.range.value.start != 0">
+                     <button class="btn" @click="pagination.previousPage" :disabled="pagination.range.value.start == 0">
                         <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2574 5.59165C11.9324 5.26665 11.4074 5.26665 11.0824 5.59165L7.25742 9.41665C6.93242 9.74165 6.93242 10.2667 7.25742 10.5917L11.0824 14.4167C11.4074 14.7417 11.9324 14.7417 12.2574 14.4167C12.5824 14.0917 12.5824 13.5667 12.2574 13.2417L9.02409 9.99998L12.2574 6.76665C12.5824 6.44165 12.5741 5.90832 12.2574 5.59165Z" fill="#969696" />
                         </svg>
@@ -88,7 +89,7 @@
                      <!-- <button class="btn btn-active">1</button> -->
                      <button :class="`btn ${i != pagination.currentPageNumber.value || 'btn-active'}`" v-for="i in pagination.numberOfPages" @click="pagination.goToPage(i)">{{ i }}</button>
                      <!-- <button class="btn">3</button> -->
-                     <button class="btn" @click="pagination.nextPage" v-if="pagination.range.value.end <= jobs.length">
+                     <button class="btn" @click="pagination.nextPage" :disabled="pagination.currentPageNumber.value == pagination.numberOfPages">
                         <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                            <path fill-rule="evenodd" clip-rule="evenodd" d="M7.74375 5.2448C7.41875 5.5698 7.41875 6.0948 7.74375 6.4198L10.9771 9.65314L7.74375 12.8865C7.41875 13.2115 7.41875 13.7365 7.74375 14.0615C8.06875 14.3865 8.59375 14.3865 8.91875 14.0615L12.7437 10.2365C13.0687 9.91147 13.0687 9.38647 12.7437 9.06147L8.91875 5.23647C8.60208 4.9198 8.06875 4.9198 7.74375 5.2448Z" fill="#969696" />
                         </svg>
