9f67663b94ac4b0e497f63da5c6992aab3698b52
Elliot Morrison <elliotmorrison58@gmail.com>
2023-04-07 11:44:40 -0500

    gets user data

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Candidate;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class CandidateController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return Inertia::render("Candidates");
+        return Inertia::render("Candidates",[
+            "candidates" => User::get()
+        ]);
     }
 }
