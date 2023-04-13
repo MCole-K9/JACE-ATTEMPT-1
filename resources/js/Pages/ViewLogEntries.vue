d9dc29ef632ae410947e8a5ed2544abc557ec659
khalid sheriff <khalalimsheriff@gmail.com>
2023-04-12 23:57:11 -0500

    corrective changes for logs; replaced the window.open thing with something more sensible

--- resources/js/Pages/ViewLogEntries.vue
+++ resources/js/Pages/ViewLogEntries.vue
@@ -6,18 +6,29 @@
     import type {UserActivityLog} from '../Lib/types';
     import {ref, type Ref} from 'vue';
     import Papa from 'papaparse';
+import { objectToString } from '@vue/shared';
 
     const props = defineProps<{logs: UserActivityLog[]}>();
     const isLogArchive: Ref<boolean> = ref(false);
     const logFile: Ref<UserActivityLog[] | undefined> = ref();
-
+    const isCurrentLogTab: Ref<Boolean> = ref(true);
     
-    function ExportLogs(){
-        // i am legitimately surprised this works
-        const url = window.location.origin;
-        var link = document.createElement('a');
-        link.href = `${url}/api/logs/download`;
-        link.click();
+    function viewLogArchiveTab(){
+        isCurrentLogTab.value = false;
+    }
+
+    function viewCurrentLogTab(){
+        isCurrentLogTab.value = true;
+    }
+
+    async function ExportLogs(){
+        // this is significantly more sane
+        const response = await fetch('/api/logs/download');
+        const logsBlob = await response.blob();
+
+        let a = document.createElement('a');
+        a.href = URL.createObjectURL(logsBlob);
+        a.click();
     };
 
     function UploadLogsCsv(event: Event){
@@ -34,38 +45,61 @@
                     if (typeof reader.result === "string"){
                         var readerResult = reader.result;
 
-                        Papa.parse(readerResult, {header: true, complete: (results, file)=>{
+                        Papa.parse(readerResult, {complete: (results, file)=>{
                             console.log(results.data);
 
+                            // this is somehow even more hackier, but it's typesafe at least ig
+                            type resultsObject = {0: number, 1: string, 2: string, 3: number, 4: number, 5: string}
+
+                            let resultsArray: Array<resultsObject> = results.data as resultsObject[];
+                            let logsArray: UserActivityLog[] = [];
+
+                            resultsArray.forEach(row => {
+                                let activityRecord: UserActivityLog = {} as UserActivityLog;
+
+                                activityRecord.id = row['0'];
+                                activityRecord.name = row['1'];
+                                activityRecord.description = row['2'];
+                                activityRecord.subject = row['3'];
+                                activityRecord.causer = row['4']
+                                activityRecord.timestamp = row['5'];
+
+                                logsArray.push(activityRecord);
+                            });
+
+                            console.log(logFile);
+
+                            logFile.value = logsArray;
                             isLogArchive.value = true;
                         }});
                     }
                 }
-
             }
         }
-
     };
 </script>
 <template>
     <DashboardLayout>
         <h1>User Logs</h1>
-        
-        <div class="flex flex-row">
-            <button class="btn" @click="ExportLogs">Print Logs</button>
-            <div class="flex flex-col">
+
+        <div>
+            <button class="btn" @click="viewCurrentLogTab">View Current Logs</button>
+            <button class="btn" @click="viewLogArchiveTab">View Log Archive</button>
+        </div>
+        <div>
+            <!--Shows by default-->
+            <div v-if="isCurrentLogTab">
+                <button class="btn" @click="ExportLogs">Print Logs</button>
+                <LogTable :logs="logs"/>
+            </div>
+            <!--(Ideally) should only show if the user clicks the button for it-->
+            <div v-if="!isCurrentLogTab">
                 <label for="filedialog">
                     <span>Upload Log Archive</span>
                 </label>
                 <input id="filedialog" type="file" accept="text/csv" @change="UploadLogsCsv"/>
+                <LogArchiveTable :logs="logFile"/>
             </div>
         </div>
-
-        <!--Shows by default-->
-        <LogTable :logs="logs" v-if="!isLogArchive"/>
-
-        <!--(Ideally) should only show if the user selects a csv file-->
-        <LogArchiveTable :logs="logFile" v-if="isLogArchive"/>
-
     </DashboardLayout>
 </template>
