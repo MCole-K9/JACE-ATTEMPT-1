358c56272466df405c30dd94df254a35bb03dd66
khalid sheriff <khalalimsheriff@gmail.com>
2023-04-16 17:59:56 -0500

    minor modifications to add header and footer; made reports save correctly

--- app/custom/InfractionPDF.php
+++ app/custom/InfractionPDF.php
@@ -8,10 +8,18 @@ use Codedge\Fpdf\Fpdf\Fpdf;
 class InfractionPdf extends Fpdf {
     public $month = '01';
 
-
     function Header(){
-        $this->SetFont('Times', 'B', 12);
-        $this->Cell(0, 0, 'Job Ace Infraction Report for ' . $this->month . '/' . date('Y'));
+        $this->SetFont('Times', 'B', 18);
+        $this->Cell(0, 0, 'Job Ace Infraction Report for ' . $this->month . '/' . date('Y'), 0, 0, 'C');
+        $this->Line(25, 25, 270, 25);
+        $this->Ln();
+    }
+
+    function Footer(){
+        $this->SetY(-15);
+        $this->Line(25, 185, 270, 185);
+        $this->SetFont('Times', '', 12);
+        $this->Cell(265, 0,$this->PageNo(), 0, 0, 'R');
     }
 }
 
